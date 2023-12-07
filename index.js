6f30e28e640f3b0f3cbcc4947e708e1f7cf89369
Marwan Allisy <mrwan311@gmail.com>
2023-12-07 10:17:06 +0200

    testing innit

--- index.js
+++ index.js
@@ -46,42 +46,55 @@ app.get("/api/scrape", async (req, res) => {
  const parts = user_url.pathname.split("/");
  const username = parts[1];
 
- const userDataOptions = {
-  method: "GET",
-  url: `https://twitter-api-v1-1-enterprise.p.rapidapi.com/base/apitools/${
-   scrapeOption === "followers" ? "followersList" : "followingsList"
-  }`,
-  params: {
-   apiKey:
-    "2WLXZk9kacMXhHxj2cRg19bsuJ98XiQL0ndQ94kMdciuz|1574242047661207552-3jI04wPRb0tVkUfFjR4VzJW19ZnQz3",
-   screenName: username,
-   cursor: "-1",
-  },
-  headers: {
-   "X-RapidAPI-Key": "0b85afc0b5msh13b76112a3083e1p107698jsn711d5b3650b2",
-   "X-RapidAPI-Host": "twitter-api-v1-1-enterprise.p.rapidapi.com",
-  },
- };
+ let users = [];
+ let nextPageId = "-1";
 
- const userData = await axios.request(userDataOptions);
- const parsedData = await JSON.parse(userData.data.data);
- console.log(parsedData.users.length);
- if (!parsedData) return;
+ while (nextPageId != 0) {
+  const userDataOptions = {
+   method: "GET",
+   url: `https://twitter-api-v1-1-enterprise.p.rapidapi.com/base/apitools/${
+    scrapeOption === "followers" ? "followersList" : "followingsList"
+   }`,
+   params: {
+    apiKey:
+     "2WLXZk9kacMXhHxj2cRg19bsuJ98XiQL0ndQ94kMdciuz|1574242047661207552-3jI04wPRb0tVkUfFjR4VzJW19ZnQz3",
+    screenName: username,
+    cursor: nextPageId,
+   },
+   headers: {
+    "X-RapidAPI-Key": "0b85afc0b5msh13b76112a3083e1p107698jsn711d5b3650b2",
+    "X-RapidAPI-Host": "twitter-api-v1-1-enterprise.p.rapidapi.com",
+   },
+  };
 
- const users = parsedData.users?.map(
-  ({ id_str, name, location, description, followers_count, verified }) => {
-   return {
-    id: id_str,
-    name,
-    location,
-    description: description.toLowerCase(),
-    followers: followers_count,
-    verified,
-   };
-  }
- );
- console.log(users.length);
+  const userData = await axios.request(userDataOptions);
+  // console.log(userData.next_cursor_str);
+  const parsedData = await JSON.parse(userData.data.data);
 
+  console.log(await parsedData.next_cursor_str);
+
+  if (!parsedData) return;
+
+  const parsedUserData = parsedData.users?.map(
+   ({ id_str, name, location, description, followers_count, verified }) => {
+    return {
+     id: id_str,
+     name,
+     location,
+     description: description.toLowerCase(),
+     followers: followers_count,
+     verified,
+    };
+   }
+  );
+
+  nextPageId = parsedData.next_cursor_str;
+  // console.log(parsedUserData);
+  console.log(parsedUserData.length);
+  users = [...users, ...parsedUserData];
+ }
+
+ console.log("users fetched: ", users.length);
  const user = await User.findById(userId).select("_id users_DMed liveUpdate");
 
  const op = await Op.create({
@@ -103,9 +116,10 @@ app.get("/api/scrape", async (req, res) => {
 
  console.log(op);
 
+ console.log("parsing started");
  const sortedUsers = sortUsers(users, {
   followerRange,
-  excludeBioWords,
+  bioExclude: excludeBioWords,
   usersDMed: user.usersDmed,
   bioIncludes,
   verified,
@@ -113,7 +127,7 @@ app.get("/api/scrape", async (req, res) => {
  });
 
  // console.log(sortedUsers);
- console.log(sortedUsers.length);
+ console.log("found prospects: ", sortedUsers.length);
 
  // await User.findOneAndUpdate(
  //  { _id: user._id },
