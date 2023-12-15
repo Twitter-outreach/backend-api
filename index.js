44141df79664e196113d3abd821fd0fc4d6e8e6c
Marwan Allisy <mrwan311@gmail.com>
2023-12-15 09:09:17 +0200

    remodeling done

--- index.js
+++ index.js
@@ -50,26 +50,26 @@ app.get("/api/scrape", async (req, res) => {
   // tokens: profile.authTokens,
  });
 
- await User.findOneAndUpdate(
-  { _id: userId },
-  {
-   $push: {
-    operations: op._id,
-   },
-  }
- );
+ // await User.findOneAndUpdate(
+ //  { _id: userId },
+ //  {
+ //   $push: {
+ //    operations: op._id,
+ //   },
+ //  }
+ // );
 
- console.log(op);
+ // console.log(op);
 
- await Profile.findOneAndUpdate(
-  { _id: profileId },
-  {
-   status: "RUNNING",
-   $push: {
-    operations: op._id,
-   },
-  }
- );
+ // await Profile.findOneAndUpdate(
+ //  { _id: profileId },
+ //  {
+ //   status: "RUNNING",
+ //   $push: {
+ //    operations: op._id,
+ //   },
+ //  }
+ // );
 
  const user_url = new URL(url);
  const parts = user_url.pathname.split("/");
@@ -80,45 +80,41 @@ app.get("/api/scrape", async (req, res) => {
  let limitCount = 0;
  let scrapedUserId;
 
+ let sentDMs = 0;
+ let totalDMsSent = 0;
+ let usersDMedToday = [];
+
+ const user = await User.findById(userId).select("_id usersDMed liveUpdate");
+ const profileData = await Profile.findById(profileId).select("_id authTokens");
+
+ console.log(profileData);
+
  while (nextPageId != 0 || limitCount > 500) {
-  // if (!scrapedUserId) {
-  //  const verifiedOptions = {
-  //   method: "GET",
-  //   url: "https://twitter.utools.me/api/base/apitools/userByScreenNameV2",
-  //   params: {
-  //    apiKey:
-  //     "NJFa6ypiHNN2XvbeyZeyMo89WkzWmjfT3GI26ULhJeqs6|1539340831986966534-8FyvB4o9quD9PLiBJJJlzlZVvK9mdI",
-  //    screenName: username,
-  //   },
-  //   headers: { accept: "*/*" },
-  //  };
-  //  axios.request(verifiedOptions);
-  //  const scrapedUserData = await axios.request(verifiedOptions);
+  const op = await Op.findById(opId).select("_id status");
+  let dateObj = new Date();
+  let day = dateObj.getDate();
+  let month = dateObj.getMonth() + 1; // months from 1-12
+  let year = dateObj.getFullYear();
+  let currentDate = day + "/" + month + "/" + year;
+  console.log(currentDate);
 
-  //  const parsedScrapedUserData = await JSON.parse(scrapedUserData.data.data);
+  if (op.status === "TERMINATED") {
+   await Profile.findOneAndUpdate(
+    { _id: profile._id },
+    {
+     $push: {
+      statistics: {
+       date: currentDate,
+       usersDMed: usersDMedToday,
+       usersResponded: [],
+      },
+     },
+    }
+   );
 
-  //  // console.log(parsedScrapedUserData.data.user.result);
-  //  console.log(parsedScrapedUserData.data.user.result.rest_id);
-  //  scrapedUserId = parsedScrapedUserData.data.user.result.rest_id;
-  // }
-  // let options = {};
-  // if (verified === "true") {
-  //  options = {
-  //   method: "GET",
-  //   url: "https://twitter2.good6.top/api/base/apitools/blueVerifiedFollowersV2",
-  //   params: {
-  //    apiKey:
-  //     "NJFa6ypiHNN2XvbeyZeyMo89WkzWmjfT3GI26ULhJeqs6|1539340831986966534-8FyvB4o9quD9PLiBJJJlzlZVvK9mdI",
-  //    userId: scrapedUserId,
-  //    cursor: nextPageId,
-  //   },
-  //   headers: { accept: "*/*" },
-  //  };
-
-  // const userData = await axios.request(options);
-
-  // const parsedData = await JSON.parse(userData.data);
-  // } else {
+   console.log("SCRAPE TERMINATED");
+   return;
+  }
   options = {
    method: "GET",
    url: `https://twitter2.good6.top/api/base/apitools/${
@@ -133,46 +129,13 @@ app.get("/api/scrape", async (req, res) => {
    headers: { accept: "*/*" },
   };
 
-  // const userData = await axios.request(options);
-
-  // const parsedData = await JSON.parse(userData.data.data);
-  // }
-
   const userData = await axios.request(options);
 
-  // console.log(userData);
-
-  let parsedData;
-  // if (verified === "true") {
-  //  const res = await JSON.parse(userData.data.data);
-  //  // console.log("res log 1: ", res);
-  //  // parsedData = res.data.user.result.timeline.timeline.instructions.entries;
-  //  parsedData = res.data.user.result.timeline.timeline.instructions[3].entries;
-  //  // console.log("res log 2: ", parsedData);
-  // } else {
-  parsedData = await JSON.parse(userData.data.data);
-  // }
+  const parsedData = await JSON.parse(userData.data.data);
 
   if (!parsedData) return;
 
-  let parsedUserData;
-  // if (verified === "true") {
-  //  parsedUserData = parsedData.map((user) => {
-  //   if (user?.content?.clientEventInfo) {
-  //    // console.log(user.content.itemContent?.user_results?.result);
-  //    const u = user.content.itemContent.user_results.result.legacy;
-
-  //    return {
-  //     id: user.content.itemContent.user_results.result.rest_id,
-  //     name: u.name,
-  //     location: u.location,
-  //     description: u.description.toLowerCase(),
-  //     followers: u.followers_count,
-  //    };
-  //   } 
-  //  });
-  // } else {
-  parsedUserData = parsedData.users?.map(
+  const parsedUserData = parsedData.users?.map(
    ({ id_str, name, location, description, followers_count }) => {
     return {
      id: id_str,
@@ -183,52 +146,87 @@ app.get("/api/scrape", async (req, res) => {
     };
    }
   );
-  // }
-  // console.log(parsedData[parsedData.length - 1]);
-  // nextPageId = parsedData[parsedData.length - 1].content.value;
   nextPageId = parsedData.next_cursor_str;
   console.log(parsedUserData?.length);
   limitCount++;
-  users = [...users, ...parsedUserData];
-  console.log(users.length);
+
+  // users = [...users, ...parsedUserData];
+  users = [...parsedUserData];
+
+  // console.log("users: ", users);
+
+  console.log("users fetched: ", users.length);
+  console.log("parsing...");
+
+  const sortedUsers = sortUsers(users, {
+   followerRange,
+   bioExclude: excludeBioWords,
+   usersDMed: user.usersDMed,
+   bioIncludes,
+   excludeLocation,
+  });
+
+  // console.log(sortedUsers);
+  console.log("found prospects: ", sortedUsers.length);
+
+  // Send dms to those found people
+
+  // console.log(`Scrape initialized: Parsing thru`);
+  const currentUserId = JSON.stringify(user._id);
+  const opId = op._id.toString();
+
+  await sendDMs(
+   sortedUsers,
+   profileData,
+   salesLetter,
+   opId,
+   currentUserId,
+   sentDMs,
+   totalDMsSent,
+   usersDMedToday
+  );
+
+  console.log("another one.");
  }
+ let dateObj = new Date();
+ let day = dateObj.getDate();
+ let month = dateObj.getMonth() + 1; // months from 1-12
+ let year = dateObj.getFullYear();
+ let currentDate = day + "/" + month + "/" + year;
 
- console.log("users: ", users);
- console.log("users fetched: ", users.length);
- const user = await User.findById(userId).select("_id usersDMed liveUpdate");
-
- console.log("parsing started");
- const sortedUsers = sortUsers(users, {
-  followerRange,
-  bioExclude: excludeBioWords,
-  usersDMed: user.usersDMed,
-  bioIncludes,
-  excludeLocation,
- });
-
- // console.log(sortedUsers);
- console.log("found prospects: ", sortedUsers.length);
-
- await User.findOneAndUpdate(
-  { _id: user._id },
+ await Profile.findOneAndUpdate(
+  { _id: profileData._id },
   {
    $push: {
-    live_updates: {
-     messageType: "start_initialization",
-     message: `Scape initialized: ${sortedUsers.length} prospects found`,
+    statistics: {
+     date: currentDate,
+     usersDMed: usersDMedToday,
+     usersResponded: [],
     },
    },
   }
  );
 
- console.log(`Scrape initialized: ${sortedUsers.length} prospects found`);
- const currentUserId = JSON.stringify(user._id);
+ const opData = await Op.findById(opId).select("_id status");
 
- const opId = op._id.toString();
+ console.log(currentDate);
 
- const profile = await Profile.findById(profileId).select("_id authTokens");
- await sendDMs(sortedUsers, profile, salesLetter, opId, currentUserId);
+ if (opData.status === "PENDING") {
+  await Profile.findOneAndUpdate(
+   { _id: profileData._id },
+   {
+    $push: {
+     statistics: {
+      date: currentDate,
+      usersDMed: usersDMedToday,
+      usersResponded: [],
+     },
+    },
+   }
+  );
 
+  console.log("statistics recorded!");
+ }
  const totalDms = await Op.findById(op.id).select("usersDMed");
 
  await User.findOneAndUpdate(
@@ -265,19 +263,8 @@ app.get("/api/record", async (req, res) => {
  await connectToDB();
 
  const profiles = await Profile.find({});
- // .populate({
- //  path: "profile",
- //  model: Profile,
- //  select: "_id statistics authTokens",
- // });
-
- // console.log(ops);
 
  profiles.map(async (profile) => {
-  // const profile = op.profile;
-
-  // console.log(profile);
-
   const options = { method: "GET", headers: { accept: "*/*" } };
 
   let totalDms = [];
@@ -335,14 +322,10 @@ app.get("/api/record", async (req, res) => {
        id: user.id,
        name: user.name,
       });
-      // console.log("this user has replied: ", user.name);
      }
     }
    });
 
-   // console.log(dayData);
-
-   // const users = day.usersDMed;
    let uniqueDMedUsers = [
     ...new Map(DMedUsers.map((user) => [user.id, user])).values(),
    ];
@@ -360,9 +343,6 @@ app.get("/api/record", async (req, res) => {
    userResponded = [];
 
    console.log(DMedUsers, userResponded);
-
-   // console.log(uniqueUsers);
-   // day.usersResponded = uniqueUsers;
   });
 
   await Profile.findByIdAndUpdate(
@@ -376,31 +356,5 @@ app.get("/api/record", async (req, res) => {
   days = [];
   DMedUsers = [];
   userResponded = [];
-  // console.log(day);
  });
-
- // op.usersDMed.forEach((user) => {
- //  for (let message of dms) {
- //   if (
- //    message?.message?.message_data.sender_id &&
- //    message?.message?.message_data.sender_id === user.id
- //   ) {
- //    let client = message.message?.message_data;
- //    console.log(client);
- //    dmsRepliedId.push({
- //     id: client.sender_id,
- //    });
- //   }
- //  }
- // });
-
- //  dmsRepliedId = [...new Set(dmsRepliedId)];
- //  console.log(dmsRepliedId);
-
- //  await Op.findOneAndUpdate(
- //   { _id: op._id },
- //   {
- //    usersResponded: [...dmsRepliedId],
- //   }
- //  );
 });
